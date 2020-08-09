e8efd42fa87acf5adf20e2a1189a5ea5e3f6ab32
raccoon86 <onehub86@gmail.com>
2020-08-09 18:33:37 +0900

    Search 기능 개선
      - 광역시 이외 도시 자동 검색 기능 추가.

--- places/templatetags/get_region.py
+++ places/templatetags/get_region.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Q
 from places import models as places_models
 
 register = template.Library()
@@ -9,5 +10,7 @@ def get_region():
 
     search = []
     search += places_models.Region.objects.all()
+    for i in range(31, 39):
+        search += places_models.Sub_Region.objects.filter(region__code=f"{i}")
 
     return search
